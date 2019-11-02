50f933c00f03716d861a3bfa206a285853249f35
rofe-dl <sheikh.md.rafidul.islam@g.bracu.ac.bd>
2019-11-02 21:42:46 +0600

    half done with Scene Switcher

--- UITraining/src/SceneSwitcher/SceneSwitcher.java
+++ UITraining/src/SceneSwitcher/SceneSwitcher.java
@@ -10,7 +10,7 @@ import javafx.event.*;
 public class SceneSwitcher extends Application{
 
     Label whatWillBeAdded, calcResult;
-    Button btnAddA, btnSubA, btnAddB, btnSubB, switchToCounter;
+    Button btnAddA, btnSubA, btnAddB, btnSubB, calculate, switchToCounter;
     int a = 0, b = 0, result = a + b;
 
     Label counterLabel;
@@ -20,29 +20,47 @@ public class SceneSwitcher extends Application{
 
     @Override
     public void start(Stage primaryStage){
-        stage = primaryStage;
+        stage = primaryStage;//so the stage is accessible in private methods
 
-        whatWillBeAdded = new Label("You will add " + a + " and " + b);
+        // CODE DEALING WITH ADDER SOFTWARE
+
+        whatWillBeAdded = new Label("You will add " + a + " and " + b);//initializes labels
         calcResult = new Label("Calculation: " + result);
-        btnAddA = new Button("+A"); btnSubA = new Button("-A");
+
+        btnAddA = new Button("+A"); btnSubA = new Button("-A");//initializes buttons
         btnAddB = new Button("+B"); btnSubB = new Button("-B");
+        calculate = new Button("Calculate");
         switchToCounter = new Button("Switch !");
 
-        HBox buttons = new HBox(10); HBox.setMargin(buttons, new Insets(10));
-        buttons.getChildren().addAll(btnAddA, btnSubA, btnAddB, btnSubB); buttons.setSpacing(15);
-        BorderPane adderLabelPos = new BorderPane(whatWillBeAdded), switcherPos = new BorderPane(switchToCounter)
-                , calcResultPos = new BorderPane(calcResult);
+        HBox buttons = new HBox(15);//creates the hbox that will hold line of buttons
+        Region spacer1 = new Region(), spacer2 = new Region(); HBox.setHgrow(spacer1, Priority.ALWAYS); HBox.setHgrow(spacer2, Priority.ALWAYS);//creates growing regions
+        buttons.getChildren().addAll(spacer1, btnAddA, btnSubA, btnAddB, btnSubB, spacer2);//adds the regions and buttons to the hbox
 
-        VBox box = new VBox(); box.getChildren().addAll(adderLabelPos, buttons, switcherPos, calcResultPos);
-        box.setSpacing(15);
-        Scene scene = new Scene(box);
+        //creates borderpanes for other nodes
+        VBox box = new VBox(15); box.getChildren().addAll(new BorderPane(whatWillBeAdded), buttons, new BorderPane(calculate), new BorderPane(calcResult),
+                new BorderPane(switchToCounter));
 
-        stage.setScene(scene);
+        box.setPadding(new Insets(10));
+        Scene scene = new Scene(box, 500, 190);
+
+        stage.setScene(scene); stage.setResizable(false);
         stage.setTitle("Scene Switcher");
         stage.show();
 
-        btnAddA.setOnAction(e -> increaseA()); btnSubA.setOnAction(e -> decreaseA());
+        btnAddA.setOnAction(e -> increaseA()); btnSubA.setOnAction(e -> decreaseA());//sets the functions for the buttons
         btnAddB.setOnAction(e -> increaseB()); btnSubB.setOnAction(e -> decreaseB());
+        calculate.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                result = a + b;
+                calcResult.setText("Calculation: " + result);
+            }
+        });
+
+        // CODE DEALING WITH COUNTER SOFTWARE
+
+
+
         switchToCounter.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
