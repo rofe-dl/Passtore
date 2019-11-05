a564a08ccf0b250984afb6d7f65b8439b9f275a6
rofe-dl <sheikh.md.rafidul.islam@g.bracu.ac.bd>
2019-11-05 21:36:32 +0600

    ok

--- UITraining/src/mainapp/MainApp.java
+++ UITraining/src/mainapp/MainApp.java
@@ -1,6 +1,7 @@
 package mainapp;
 
 import javafx.application.Application;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
@@ -96,9 +97,48 @@ public class MainApp extends Application {
     }
 
     /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     *
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
      * Returns the main stage.
      * @return
      */
+
     public Stage getPrimaryStage() {
         return primaryStage;
     }
