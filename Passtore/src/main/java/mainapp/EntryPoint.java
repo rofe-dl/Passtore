dc836fa6c3043d3b5e64b1c0fac719836a6bcdc1
rofe-dl <sheikh.md.rafidul.islam@g.bracu.ac.bd>
2019-11-15 16:11:39 +0600

    dk if fixed or not

---
+++ Passtore/src/main/java/mainapp/EntryPoint.java
@@ -0,0 +1,7 @@
+package mainapp;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        Passtore.main(args);
+    }
+}
--- Passtore/src/main/java/mainapp/StartingPoint.java
+++
@@ -1,7 +0,0 @@
-package mainapp;
-
-public class StartingPoint {
-    public static void main(String[] args) {
-        Passtore.main(args);
-    }
-}
